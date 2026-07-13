ff12f27e9152b04d5ae5d9bfccf99f85cadcc8e2
hoangnt.dev <hoangnt.dev@gmail.com>
2026-07-13 17:45:13 +0700

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "@/tw";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-gray-900">
+        Edit app/index.tsx to edit this screen. Hello sss
+      </Text>
+    </View>
+  );
+}
